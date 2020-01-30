199d872c005eaf449521d18065affc5bac10e8ac
Anthony Lopez <anthonybeconscious@gmail.com>
2020-01-30 10:53:04 -0600

    mvpish

---
+++ api/server.js
@@ -0,0 +1,21 @@
+  
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const userRouter = require('../users/users-router');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.use('/api', userRouter);
+
+
+server.get('/', (req, res) => {
+  res.send("Welcome in...");
+});
+
+module.exports = server;
